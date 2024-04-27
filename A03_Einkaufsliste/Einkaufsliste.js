3053d6820eed821b6f6ab21c21c0cb31a1621b37
firatsel <149698005+firatsel@users.noreply.github.com>
2024-04-27 22:07:34 +0200

    A04

--- A03_Einkaufsliste/Einkaufsliste.js
+++ A03_Einkaufsliste/Einkaufsliste.js
@@ -1,3 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.data = void 0;
+exports.data = [
+    { product: "Apfel", checked: "clicked", category: "Obst und Gemüse", date: "25.04.2024", amount: 1,
+        comment: " ", delete: "clicked" },
+    { product: "Joghurt", checked: "clicked", category: "Milch und Käse", date: "25.04.2024", amount: 1,
+        comment: " ", delete: "clicked" },
+    { product: "Kuchen", checked: "clicked", category: "Backwaren", date: "25.04.2024", amount: 1,
+        comment: " ", delete: "clicked" },
+    { product: "Waschmittel", checked: "clicked", category: "Haushaltsmittel", date: "25.04.2024", amount: 2,
+        comment: " ", delete: "clicked" }
+];
+for (var _i = 0, data_1 = exports.data; _i < data_1.length; _i++) {
+    var entry = data_1[_i];
+    console.log(entry);
+}
 function myFunction() {
     var x = document.getElementById("warensuche");
     var y = document.getElementById("kategorie");
@@ -6,27 +23,31 @@ function myFunction() {
     console.log(y.value);
     console.log(z.value);
 }
-function myCheck() {
+function boughtButton() {
     var myElement = document.getElementById("feld01");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
-function myCheck02() {
+function boughtButton02() {
     var myElement = document.getElementById("feld02");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
-function myCheck03() {
+function boughtButton03() {
     var myElement = document.getElementById("feld03");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
-function myCheck04() {
+function boughtButton04() {
     var myElement = document.getElementById("feld04");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
+}
+function deleteButton04() {
+    var myElement = document.getElementById("feld04");
+    myElement.remove();
 }
--- A03_Einkaufsliste/Einkaufsliste.ts
+++ A03_Einkaufsliste/Einkaufsliste.ts
@@ -1,3 +1,19 @@
+export let data = 
+    [
+        {product: "Apfel", checked: "clicked", category: "Obst und Gemüse", date: "25.04.2024", amount: 1, 
+        comment: " ", delete: "clicked"},
+        {product: "Joghurt", checked: "clicked", category: "Milch und Käse", date: "25.04.2024", amount: 1, 
+        comment: " ", delete: "clicked"},
+        {product: "Kuchen", checked: "clicked", category: "Backwaren", date: "25.04.2024", amount: 1, 
+        comment: " ", delete: "clicked"},
+        {product: "Waschmittel", checked: "clicked", category: "Haushaltsmittel", date: "25.04.2024", amount: 2, 
+        comment: " ", delete: "clicked"}
+    ]
+    
+for (let entry of data) {
+    console.log(entry);
+}
+
 function myFunction() {
     let x = <HTMLInputElement>document.getElementById("warensuche");
     let y = <HTMLInputElement>document.getElementById("kategorie");
@@ -7,30 +23,35 @@ function myFunction() {
     console.log(z.value);
 }
 
-function myCheck() {
+function boughtButton() {
     let myElement = <HTMLElement>document.getElementById("feld01");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
 
-function myCheck02(){
+function boughtButton02() {
     let myElement = <HTMLElement>document.getElementById("feld02");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
 
-function myCheck03(){
+function boughtButton03() {
     let myElement = <HTMLElement>document.getElementById("feld03");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
 }
 
-function myCheck04(){
+function boughtButton04() {
     let myElement = <HTMLElement>document.getElementById("feld04");
     myElement.style.background = "green";
     myElement.style.color = "white";
     console.log(new Date().toLocaleString());
+}
+
+function deleteButton04() {
+    let myElement = <HTMLElement>document.getElementById("feld04");
+    myElement.remove();
 }
